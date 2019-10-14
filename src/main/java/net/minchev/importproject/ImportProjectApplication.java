510a6fffed70311b1d8040eafc8e09a25c02339d
Georgi Minchev <georgi.minchev@scalefocus.com>
2019-10-14 16:53:55 +0300

    last changes

--- src/main/java/net/minchev/importproject/ImportProjectApplication.java
+++ src/main/java/net/minchev/importproject/ImportProjectApplication.java
@@ -1,5 +1,6 @@
 package net.minchev.importproject;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,8 @@ public class ImportProjectApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(ImportProjectApplication.class, args);
+		Logger logger = LoggerFactory.getLogger(ImportProjectApplication.class);
+		logger.info("ImportProject");
 
 	}
 
--- src/main/java/net/minchev/importproject/ShopController.java
+++ src/main/java/net/minchev/importproject/ShopController.java
@@ -7,6 +7,5 @@ public class ShopController {
 
     public ShopController() {
         System.out.println("Boza");
-
     }
 }
