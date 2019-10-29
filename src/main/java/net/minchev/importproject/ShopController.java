5e0316569ae9dd1c4f5c9270958eba8462cef901
Georgi Minchev <georgi.minchev@scalefocus.com>
2019-10-29 10:00:43 +0200

    add test

--- src/main/java/net/minchev/importproject/ShopController.java
+++ src/main/java/net/minchev/importproject/ShopController.java
@@ -1,10 +1,12 @@
 package net.minchev.importproject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ShopController {
 
+    @Autowired
     public ShopController() {
         System.out.println("Boza");
     }
